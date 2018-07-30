1e4bf55b56eb50e61dfe5cd52cf1df9991192bf5
rakeshsai28 <rakeshsai28@gmail.com>
2018-07-30 17:51:42 -0500

    Commit All and Push

--- Patient_UI/Home.Designer.cs
+++ Patient_UI/Home.Designer.cs
@@ -83,7 +83,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(728, 296);
             this.dataGridView1.TabIndex = 4;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+
             // 
             // username
             // 
